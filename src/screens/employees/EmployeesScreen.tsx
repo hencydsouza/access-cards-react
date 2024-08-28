c18acf6b4a987fa90824c4e55868f05f34b5ac3f
Hency Dsouza <hencyhd39@gmail.com>
2024-08-28 17:49:35 +0530

    feat: display employees as table on large screen

--- src/screens/employees/EmployeesScreen.tsx
+++ src/screens/employees/EmployeesScreen.tsx
@@ -14,6 +14,10 @@ const EmployeesScreen = () => {
         name: "name",
         _id: "_id"
     }])
+    const [buildingNames, setBuildingNames] = useState<{ name: string, id: string }[]>([{
+        name: "name",
+        id: "id"
+    }])
 
     const [isLoading, setIsLoading] = useState(true)
 
@@ -34,10 +38,19 @@ const EmployeesScreen = () => {
                 console.error('Error fetching company names:', error)
             }
         }
+        const fetchBuildingNames = async () => {
+            try {
+                const response = await useApiClient._getWithToken('/building/buildingNames', auth.accessToken)
+                console.log(response.data)
+                setBuildingNames(response.data)
+            } catch (error) {
+                console.error('Error fetching company names:', error)
+            }
+        }
 
         const loadData = async () => {
             setIsLoading(true)
-            await Promise.all([fetchData(), fetchCompanyNames()])
+            await Promise.all([fetchData(), fetchCompanyNames(), fetchBuildingNames()])
             setIsLoading(false)
         }
 
@@ -64,40 +77,52 @@ const EmployeesScreen = () => {
                 </div>
 
                 <div>
-                    {/* <table>
-                                    <thead>
-                                        <tr>
-                                            <th></th>
-                                            <th>Building Name</th>
-                                            <th>Owner Company</th>
-                                            <th>Location</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <div className="icon-container">
-                                                    <img src="svg/building.svg" alt="">
-                                                </div>
-                                            </td>
-                                            <td>
-                                                Ajantha Towers
-                                            </td>
-                                            <td>PaceWisdom Solutions</td>
-                                            <td>Bejai, Mangalore</td>
-                                            <td>
-                                                <div>
-                                                    <button type="button" className="btn edit-btn">
-                                                        <img src="svg/edit.svg" alt="">
-                                                            Edit
-                                                    </button>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>  */}
-                    <div className="gap-3 mt-8 grid lg:grid-cols-2 xl:grid-cols-3">
+                    <div className="lg:flex flex-col mt-8 hidden">
+                        <div className="-m-1.5 overflow-x-auto">
+                            <div className="p-1.5 min-w-full inline-block align-middle">
+                                <div className="border-[#e8f1fd] border-1 rounded-lg overflow-hidden shadow-sm">
+                                    <table className="min-w-full divide-y">
+                                        <thead className="bg-[#e8f1fd] text-[#0b3f7f] font-medium">
+                                            <tr>
+                                                <th scope="col" className="px-6 py-3 text-start text-xs">Employee Name</th>
+                                                <th scope="col" className="px-6 py-3 text-start text-xs">Company</th>
+                                                <th scope="col" className="px-6 py-3 text-start text-xs">Building</th>
+                                                <th scope="col" className="px-6 py-3 text-end text-xs">Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className="divide-y bg-white">
+                                            {
+                                                employees.map((data) => {
+                                                    return (
+                                                        <tr>
+                                                            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">{data.name}</td>
+                                                            <td className="px-6 py-4 whitespace-nowrap text-sm">{companyNames.find((item) => item._id == data.company.companyId)?.name}</td>
+                                                            <td className="px-6 py-4 whitespace-nowrap text-sm">{buildingNames.find((item) => item.id == data.company.buildingId)?.name}</td>
+                                                            <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                                                                <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold">
+                                                                    <LinkContainer to={`/dashboard/employees/${data.id}`} state={data}>
+                                                                        <Button variant="outline-primary" className="w-max flex items-center gap-2 mt-auto">
+                                                                            <i className="fa-regular fa-eye"></i>
+                                                                        </Button>
+                                                                    </LinkContainer>
+                                                                    <LinkContainer to={`/dashboard/employees/edit/${data.id}`} state={data}>
+                                                                        <Button variant="outline-primary" className="w-max flex items-center gap-2 mt-auto">
+                                                                            <i className="fa-regular fa-pen-to-square"></i>
+                                                                        </Button>
+                                                                    </LinkContainer>
+                                                                </button>
+                                                            </td>
+                                                        </tr>
+                                                    )
+                                                })
+                                            }
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="gap-3 mt-8 grid lg:grid-cols-2 xl:grid-cols-3 lg:hidden">
                         {
                             employees.map((data) => {
                                 return <EmployeeCard state={data} companyNames={companyNames} key={data.id} />
